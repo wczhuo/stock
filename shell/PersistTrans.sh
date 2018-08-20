d4479ab6b633fb30ccf2282c0923ed849a4221ee
wangcz <wangcz@2345.com>
2018-08-21 07:49:11 +0800

    定时压缩文件，节省磁盘空间

--- shell/PersistTrans.sh
+++ shell/PersistTrans.sh
@@ -17,3 +17,9 @@ fi
 /opt/app/php/bin/php /opt/case/stock/GetTransList.php ${dt} > /opt/case/stock/logs/GetTransList.txt 2>&1
 /opt/app/php/bin/php /opt/case/stock/Get163ChdData.php ${dt} > /opt/case/stock/logs/Get163ChdData.php 2>&1
 /opt/app/php/bin/php /opt/case/stock/PersistTransToMysql.php ${dt} > /opt/case/stock/logs/PersistTransToMysql.php 2>&1
+
+# 数据压缩保存
+cd /opt/data/stock/ && tar -cjf /opt/data/stock.${dt}.tar.bz2 ${dt}/*
+
+# 删除原文件
+cd /opt/data/stock/ && rm -rf /opt/data/stock/${dt}
--- shell/PersistTransMulti.sh
+++ shell/PersistTransMulti.sh
@@ -2,12 +2,35 @@
 
 # 注意换行符必须为LF
 
-month=$1
-dateStart=$2
-dateEnd=$3
+if [ -n "$1" ]; then
+    dateStart=$1
+else
+    dateStart=$(date -d"-1 day" +"%Y-%m-%d")
+fi
 
-for dt in `seq -w ${dateStart} ${dateEnd}`
+if [ -n "$2" ]; then
+    dateEnd=$2
+else
+    dateEnd=$(date -d"-1 day" +"%Y-%m-%d")
+fi
+
+date=${dateStart}
+dateList=""
+while [ $(date -d"$date" +"%Y%m%d") -le $(date -d"$dateEnd" +"%Y%m%d") ];
 do
-    echo "/opt/app/php/bin/php /opt/case/stock/PersistTransToMysql.php 2018-${month}-${dt}"
-    /opt/app/php/bin/php /opt/case/stock/PersistTransToMysql.php 2018-${month}-${dt}
+    dateList="${dateList} ${date}"
+    date=$(date -d"+1 day $date" +"%Y-%m-%d")
+done
+dateList="${dateList}"
+
+for dt in ${dateList}
+do
+    echo ${dt}
+#    echo "/opt/app/php/bin/php /opt/case/stock/PersistTransToMysql.php ${dt}"
+#    /opt/app/php/bin/php /opt/case/stock/PersistTransToMysql.php ${dt}
+    # 数据压缩保存
+    cd /opt/data/stock/ && tar -cjf /opt/data/stock.${dt}.tar.bz2 ${dt}/*
+
+    # 删除原文件
+#    cd /opt/data/stock/ && rm -rf /opt/data/stock/${dt}
 done
